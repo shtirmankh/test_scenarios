370a2b3f66186ed6e07eb1a74ea0ce3f71a25dba
shturman <yr999@list.ru>
2014-11-12 14:29:59 +0200

    third commit

--- test_scenario2.py
+++ test_scenario2.py
@@ -3,11 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import unittest, time, re
+import unittest
+import time
 
 class TestScenario2(unittest.TestCase):
 
